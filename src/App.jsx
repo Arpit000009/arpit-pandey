ac2730206f5baedb4876aa0cee77e089ee0dd909
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-05-13 16:19:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edittt <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
