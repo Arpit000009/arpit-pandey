1ef0ad8248b576f5f468bc35002486a7d3df10f1
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-05-14 04:46:46 +0530

    hero2

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,13 +2,18 @@ import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/hero-img.png'
 import themeIcon from '../../assets/sun.svg'
 import leetcodeIcon from '../../assets/icons8-leetcode-50.png'
+import githubIcon from '../../assets/github-light.svg'
+import linkedinIcon from '../../assets/linkedin-light.svg'
+import CV from '../../assets/Resume_arpit_s.pdf'
+import { useTheme } from '../../common/ThemeContext'
 function Hero() {
+    const {theme,toggleTheme} = useTheme();
   return (
-    <section id='hero' className='styles.container'>
-      <div className='styles.colorModeContainer'>
+    <section id='hero' className={styles.container}>
+      <div className={styles.colorModeContainer}>
         <img className={styles.hero} src={heroImg} alt="Profile picture of Arpit Pandey" />
         <img
-         classname = {styles.color} 
+         className = {styles.color} 
          src={themeIcon} 
          alt="color mode icon" />
       </div>
@@ -22,7 +27,19 @@ function Hero() {
             <a href="https://leetcode.com/u/arpitpandey_cse21/" target='_blank'>
             <img src={leetcodeIcon} alt="leetcode icon" />
             </a>
+            <a href="https://github.com/Arpit000009" target='_blank'>
+            <img src={githubIcon} alt="github icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/arpit-pandey-b7814027b" target='_blank'>
+            <img src={linkedinIcon} alt="linkedin icon" />
+            </a>
         </span>
+        <p>with a passion for devloping mordern mernStack websites.</p>
+        <a href={CV} download>
+            <button className='hover' >
+                Resume
+            </button>
+        </a>
       </div>
     </section>
   )
